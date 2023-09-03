4184236362e0a0757faa867e762a3bcc36642e9f
league <57720048+richardson-liou@users.noreply.github.com>
2023-09-03 11:27:24 -0700

    no

--- src/_02_colors/VennDiagram/VennDiagram.pde
+++ src/_02_colors/VennDiagram/VennDiagram.pde
@@ -2,7 +2,16 @@ void setup() {
 size(600,600);
 }
 void draw(){
-ellipse(400,200,250,250);
-ellipse(200,200,250,250);
-ellipse(300,400,250,250);
+  fill(0,0,0,255);
+  text("What am I doing here",320,75);
+
+  text("Oh, I understand!", 200,75);
+
+  text("No, you don't!", 250,485);
+  fill(255,0,0,128);
+ellipse(350,200,250,250);
+  fill(0,0,255,128);
+ellipse(250,200,250,250);
+  fill(0,255,0,128);
+ellipse(300,325,250,250);
 }
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,10 +8,17 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String Liam = "Good at math";
+String Thatonekidthatanimateslegos = "He animated the lego scene in spoderverse";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("Name some one");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+if (name.equals("14")) {
+	JOptionPane.showMessageDialog(null, Thatonekidthatanimateslegos);
+}
+if (name.equals("Liam")) {
+	JOptionPane.showMessageDialog(null, Liam);
+}
 
 	}
 }
