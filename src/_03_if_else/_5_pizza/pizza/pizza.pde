d01397c31c0272f523ed2f6b3a71ed611daef87f
league <league@imac-22.attlocal.net>
2023-09-17 11:27:57 -0700

    Tasty pizza + riddles.

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,23 @@
-
+ PImage pepperoni;
+ PImage smoliv;
+ PImage mushroom;
 void setup() {
-    
+pepperoni = loadImage("pepperoni.png");
+    size(600,600);
+smoliv = loadImage("olive.png");
+mushroom =loadImage("mushroom.png");
 }
 void draw() {
-    
+    image(pepperoni,300,300);
+        image(pepperoni,200,129);
+        image(pepperoni,93,80);
+    image(smoliv,203,174);
+        image(smoliv,222,256);
+        if (keyPressed && (key=='b'|| key=='q') ) {
+             image(pepperoni,320,345); 
+             image(mushroom,256,345);
+        }
+        noStroke();
+    fill(34,212,200,130);
+    ellipse(300,300,600,600);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,12 +12,47 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String awnser1 = JOptionPane.showInputDialog(null, "I have no lung but I need to breath.=, if I touch water I die, what am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if (awnser1.equals("fire")) {
+	JOptionPane.showMessageDialog(null, "DING! Correct!");
+	score +=1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "ERRR! Incorrect.");
+	score -=1;
+}
+String awnser2 = JOptionPane.showInputDialog(null,"I am easy to lift but hard to throw what am I?");
+if (awnser2.equals("A feather")) {
+	JOptionPane.showMessageDialog(null, "DING! Correct!");
+	score +=1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "ERRR! Incorrect.");
+	score -=1;
+}
+String awnser3 = JOptionPane.showInputDialog("An electric train is heading east at 400 mph. How fast will the smoke blow?");
+if (awnser3.equals("No smoke")) {
+	JOptionPane.showMessageDialog(null, "DING! Correct!");
+	score +=1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "ERRR! Incorrect.");
+	score -=1;
+}
+String awnser4 = JOptionPane.showInputDialog("Kids can make it but never hold it or see it. What is it");
+if (awnser4.equals("Noise")) {
+	JOptionPane.showMessageDialog(null, "DING! Correct!");
+	score +=1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "ERRR! Incorrect.");
+	score -=1;
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+}
+// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
